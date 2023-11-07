15c4e195e220d5336db649f82635df5179db31ee
Hemanth Nandish <hemanthluvsfifa@gmail.com>
2023-11-07 06:01:05 +0000

    create sleep.c

---
+++ user/sleep.c
@@ -0,0 +1,19 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+
+int main(int argc, char **argv)
+{
+  if(argc != 2)
+  {
+    printf("Error: Requires one argument\n");
+    exit(1);
+  }
+
+
+
+  return 0;
+
+
+}
